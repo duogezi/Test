05d71a550dbe01a2f13308224298a81b04f8fce2
duogezi <596958876@qq.com>
2016-08-13 09:59:58 +0800

    add wpf project

---
+++ Wpf_MVVM/Wpf_MVVM/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf_MVVM
+{
+    class DelegateCommand : ICommand
+    {
+        public Action<object> ExcuteCommand = null;
+        public Func<object, bool> CanExcuteCommand = null;
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            if (CanExcuteCommand != null)
+            {
+                return CanExcuteCommand(parameter);
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (ExcuteCommand != null)
+            {
+                ExcuteCommand(parameter);
+            }
+        }
+        public void RaiseCanExcuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this,EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Wpf_MVVM/Wpf_MVVM/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_MVVM
+{
+    class ViewModel
+    {
+        public DelegateCommand CopyCommand { get; set; }
+        public Model model { get; set; }
+        public ViewModel()
+        {
+            this.model = new Model();
+            this.CopyCommand = new DelegateCommand();
+            this.CopyCommand.ExcuteCommand = new Action<object>(this.model.Copy);
+        }
+    }
+}
